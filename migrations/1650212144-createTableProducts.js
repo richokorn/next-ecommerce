8ece93debd09364010a86cdd29cdeb10bbbbc506
Richard Korn <richo.korn@gmail.com>
2022-04-17 20:08:15 +0200

    Add utils, dotenv-safe, dotenv-cli deps

--- migrations/1650212144-createTableProducts.js
+++ migrations/1650212144-createTableProducts.js
@@ -1,7 +1,19 @@
-exports.up = async client => {
-	// <insert magic here>
+exports.up = async (sql) => {
+  await sql`
+		CREATE TABLE IF NOT EXISTS products (
+			id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+			name varchar(255) NOT NULL,
+			price integer NOT NULL,
+			priceString varchar(255) NOT NULL,
+			type varchar(255) NOT NULL,
+			title varchar(255) NOT NULL,
+			description varchar(255) NOT NULL
+		)
+	`;
 };
 
-exports.down = async client => {
-	// just in case...
+exports.down = async (sql) => {
+  await sql`
+	DROP TABLE products;
+	`;
 };
